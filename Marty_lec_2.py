06fb0f94fcd997a18c6b9d9931d0c582ddd882dc
tapa8728 <tapa8728@colorado.edu>
2021-02-08 23:10:40 -0800

    Lec 2: Binary

--- Marty_lec_2.py
+++ Marty_lec_2.py
@@ -6,6 +6,24 @@ return command is very important
 You want to make sure that all paths of your code (if-else) return a value of some kind. 
 
 PrintBinary 
-Base Case - 
-Recursive Case -  
+Base Case - if n in [0,1] then return n itself ( this is easy for a single bit)
+Recursive Case -  ??
+	what is the easiest way to bite of a chunk ?
+	eg:  101101 
+	it is easiest to get the last "1" and second-last "0"
+ 	using div/mod operations. 
+
+
+
 '''
+
+def printBinary(n): # 2
+	if n <= 1: # base case 
+		print n, # 1
+	else: # recursive case 
+		prefix = n/2  # 5, 2, 1
+		lastdigit = n%2 # 1, 1, 0
+		printBinary(prefix) # 5, 2, 1
+		print lastdigit, # "1 1 0"
+
+printBinary(3) 
