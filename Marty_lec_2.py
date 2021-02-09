f6ced0ff6f044d482a6335cee5edaa58815e8986
tapa8728 <tapa8728@colorado.edu>
2021-02-08 22:40:40 -0800

    Marty Lec 2

---
+++ Marty_lec_2.py
@@ -0,0 +1,11 @@
+# https://www.bilibili.com/video/BV1B4411W7Ld?p=9
+
+'''
+return command is very important 
+
+You want to make sure that all paths of your code (if-else) return a value of some kind. 
+
+PrintBinary 
+Base Case - 
+Recursive Case -  
+'''
