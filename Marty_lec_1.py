a93ad9581f48732a925413c08f93f62e2313dccf
Tanvi Parikh <tanvi@aardvark.local>
2021-02-08 21:15:28 -0800

    Marty lec 1

---
+++ Marty_lec_1.py
@@ -0,0 +1,11 @@
+
+# https://www.bilibili.com/video/BV1B4411W7Ld?p=8 
+
+'''
+When you think about recursion, think about a bunch of small workers solving a larger problem
+
+Eg - Find the total number of students in a column.
+In this case, the workers are each of the students themselves. 
+No for loops as we dont want one worker to do all the work, instead we want each worker to do its own work and help towards the final solution
+
+'''
