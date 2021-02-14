6fd65069c86d9734f84b0ea8e34adfa5eb2af27e
tapa8728 <tapa8728@colorado.edu>
2021-02-13 18:13:45 -0800

    permutation

--- Backtracking_4.py
+++ Backtracking_4.py
@@ -1,15 +1,20 @@
 #https://www.bilibili.com/video/BV1B4411W7Ld?p=16&spm_id_from=pageDriver
 
 def permuteName(name, chosen, res):
+	print "permuteName(",name,",",chosen,")"
 	if len(name) == 0:
+		#print "inside : ", chosen
 		res.append("".join(chosen))
 	else:
-		#choose
+		# choose
 
 		# explore
+		for i in xrange(len(name)):
+
+			permuteName(name[:i]+name[i+1:], chosen + [name[i]], res )
 
 		# unchoose 
 
 res=[]
-permuteName("MARTY", [], res)
+permuteName(["M","A","R","T","Y"], [], res)
 print res
